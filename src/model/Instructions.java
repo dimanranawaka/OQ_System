ee79e4764b8e632899ddac0cbe0b1e1c07be06d7
dimanranawaka <dimanranawaka@gmail.com>
2023-08-05 17:51:46 +0530

    Implement - Instructions model

---
+++ src/model/Instructions.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Instructions {
+    private String instruction;
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+}
