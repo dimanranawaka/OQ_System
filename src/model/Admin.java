875264034d064a244f458aa1d1dd38c70c7a68ce
dimanranawaka <dimanranawaka@gmail.com>
2023-08-05 17:29:41 +0530

    Implement - Admin and Student models

---
+++ src/model/Admin.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Admin {
+    public static int pre_count =0;
+    private String adminUserName;
+    private String adminPassword;
+
+    public String getAdminUserName() {
+        return adminUserName;
+    }
+
+    public void setAdminUserName(String adminUserName) {
+        this.adminUserName = adminUserName;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+}
---
+++ src/model/Student.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Student {
+    private String studentUserName;
+    private String studentPassword;
+
+    public String getStudentUserName() {
+        return studentUserName;
+    }
+
+    public void setStudentUserName(String studentUserName) {
+        this.studentUserName = studentUserName;
+    }
+
+    public String getStudentPassword() {
+        return studentPassword;
+    }
+
+    public void setStudentPassword(String studentPassword) {
+        this.studentPassword = studentPassword;
+    }
+}
